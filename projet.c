7f8dfbf994d7166d1aba2819ac2f5320adc021aa
valzizi <valent1tag@gmail.com>
2026-05-08 19:16:03 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -24,20 +24,20 @@ typedef struct{
 
 
 
-joueur CreationJoueur(int x){ 
+joueur CreationJoueur(int x){ //creer le profil de chaque joueur de la partie
   joueur a;
-  char* pseudo[50];
+  char* pseudo[50]; // stock le pseudonyme de chaque joueur 
   a.numj=x;
   
   printf (" Quelle est le pseudo du joueur %d \n ", x);
   scanf ( " %s", a.pseudo);
   a.nb_carte=0;
   a.score=0;
-  a.score_total=0;
+  a.score_total=0;                // initialise les données du joueur 
   a.actif=1;
 
 retun a;
-}// fonction de creation des joueurs 
+} 
 
 
 
