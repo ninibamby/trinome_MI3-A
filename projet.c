d377c8c85feb05333e83532eca8f8bdca2b21956
Sifeddine95 <sifeddine.bertouli@etu.cyu.fr>
2026-05-12 18:01:02 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -404,8 +404,37 @@ for ( int i = 0; i <= etape; i++ ){
 }while ( desordre != 0 && etape > 0 );
 }
 
-void affiche_resulats( joueur* tab, int taille, int n){
+void affiche_resulat( joueur* tab, int taille, int n){
+if( tab == NULL ){
+return;
+}
+if ( taille <= 0 ){
+return;
+}
+if ( n != 1 && n != 2 ){
+return;
+}
 
+int j = 1;
+int k = 1;
+if ( n == 1 ){
+class_joueurs( tab, taille );
+printf(" On a un grand gagnant ! c'est %s avec %d points \n ", tab[taille-1].pseudo, tab[taille-1].score_total );
+printf(" Le classement des joueurs est donc : \n");
+for (int i = taille; i > 0; i--){
+printf(" Le classement du joueur %s est : %d \n", tab[i].pseudo, j );
+j++;
+}
+}else if ( n == 2 ){
+class_joueurs( tab, taille );
+printf(" Personne n'a réussit à obtenir 200 points durant cette partie mais nous avons quand même un gagnant ! " );
+printf(" Le classement des joueurs est donc : \n");
+for (int y = taille; y > 0; y--){
+printf(" Le classement du joueur %s est : %d \n", tab[i].pseudo, k );
+k++;
+}
+}
+}
 
 
 
