530fbc21c63375460e499c9347968350bf9ba419
ninibamby <nino.payet@etu.u-cergy.fr>
2026-05-13 16:51:03 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void affiche_main( joueur j){
+printf(" votre main, %s est : \n ", j.pseudo );
+for ( int i = 0; i < j.nb_carte; i++ ){
+    printf(" [%s], ", j.main[i].nom )
+}
+printf(" \n ");
+}
+
 
