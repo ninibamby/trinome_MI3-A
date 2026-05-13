e08c32e9a7418a58f77e2ddab8369fd6d68d485a
ninibamby <nino.payet@etu.u-cergy.fr>
2026-05-13 19:30:41 +0200

    Update structcarte.c

--- src/structcarte.c
+++ src/structcarte.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct { 
+    char nom[3];
+    int val;
+    int type;         //indicateur de type de carte : 0 si valeur, 1 si addition, 2 si multiplication
+}carte;               //structure d'une carte
 
