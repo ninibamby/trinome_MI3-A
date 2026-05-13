c6844b4d5c848b1e6a593fa17e96a1bf8ff226cb
valzizi <valent1tag@gmail.com>
2026-05-14 00:08:40 +0200

    Update affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -53,29 +53,27 @@ else if(type == 1 || type == 2){
 
 if(type == 0){ 
     printf("\033[%dm", couleur);
+    printf("╔═══════╗\n"); 
+    printf("| ◆  ◆ |\n");  
+    printf("|═══════|\n);                    
+    printf("|)  %d (|\n", c.val);    
+    printf("|═══════|\n");
+    printf("| ◆  ◆ |\n");
+    printf("╚═══════╝\n");
 
-    printf("╔═══════╗  
-            | ◆  ◆ |
-            |═══════|           
-            |)  %d (|     
-            |═══════|
-            | ◆  ◆ |
-            ╚═══════╝
-", c.val);
 
     printf("\033[0m ");
 }
 else if (type == 1 || type == 2){
      printf("\033[%dm", couleur);
 
-    printf("╔═══════╗  
-            | ◆  ◆ |
-            |═══════|           
-            |) +%d (|     
-            |═══════|
-            | ◆  ◆ |
-            ╚═══════╝
-", c.val);
+    printf("╔═══════╗\n");  
+    printf("| ◆  ◆ |\n");
+    printf("|═══════|\n");      
+    printf("|) %s (|\n", c.nom );          
+    printf("|═══════|\n");          
+    printf("| ◆  ◆ |\n");
+    printf("╚═══════╝\n");
 
     printf("\033[0m ");
 }
