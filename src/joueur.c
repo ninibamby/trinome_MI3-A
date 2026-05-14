a26a2d757d03d8cde2bd3644a8fb4ef06b2e6e89
ninibamby <nino.payet@etu.u-cergy.fr>
2026-05-14 14:08:33 +0200

    Update joueur.c

--- src/joueur.c
+++ src/joueur.c
@@ -25,52 +25,58 @@ return a;
 
 void class_joueurs( joueur* tab, int taille ){
 
-int desordre; int etape = n-1; int temp;
-Do{
+int desordre; 
+int etape = taille-1; 
+int temp;
+
+do{
     desordre = 0;
-for ( int i = 0; i <= etape; i++ ){
-    if( tab[i].score_final > tab[i+1].score_final ){
+  for ( int i = 0; i <= etape; i++ ){
+    if( tab[i].score_total > tab[i+1].score_total ){
     desordre = 1;
-    temp = tab[i].score_final;
-    tab[i].score_final = tab[i+1].score_final;
-    tab[i+1].score_final = temp;
+    temp = tab[i].score_total;
+    tab[i].score_total = tab[i+1].score_total;
+    tab[i+1].score_total = temp;
         etape --;
+    }
+  }
 }while ( desordre != 0 && etape > 0 );
 }
-}
-}
 
 
 
-void affiche_resulat( joueur* tab, int taille, int n){
-if( tab == NULL ){
+
+void affiche_resultat( joueur* tab, int taille, int n){
+  if( tab == NULL ){
 return;
-}
-if ( taille <= 0 ){
+  }
+  if ( taille <= 0 ){
 return;
-}
-if ( n != 1 && n != 2 ){
+  }
+  if ( n != 1 && n != 2 ){
 return;
-}
+  }
 
 int j = 1;
 int k = 1;
-if ( n == 1 ){
-class_joueurs( tab, taille );
-printf(" On a un grand gagnant ! c'est %s avec %d points \n ", tab[taille-1].pseudo, tab[taille-1].score_total );
-printf(" Le classement des joueurs est donc : \n");
-for (int i = taille; i > 0; i--){
-printf(" Le classement du joueur %s est : %d \n", tab[i].pseudo, j );
-j++;
-}
-}else if ( n == 2 ){
-class_joueurs( tab, taille );
-printf(" Personne n'a réussit à obtenir 200 points durant cette partie mais nous avons quand même un gagnant ! " );
-printf(" Le classement des joueurs est donc : \n");
-for (int y = taille; y > 0; y--){
-printf(" Le classement du joueur %s est : %d \n", tab[i].pseudo, k );
-k++;
-}
-}
+
+  if ( n == 1 ){
+  class_joueurs( tab, taille );
+  printf(" On a un grand gagnant ! c'est %s avec %d points \n ", tab[taille-1].pseudo, tab[taille-1].score_total );
+  printf(" Le classement des joueurs est donc : \n");
+    for (int i = taille-1; i > 0; i--){
+    printf(" Le classement du joueur %s est : %d \n", tab[i].pseudo, j );
+    j++;
+    }
+  }
+  else if ( n == 2 ){
+  class_joueurs( tab, taille );
+  printf(" Personne n'a réussit à obtenir 200 points durant cette partie mais nous avons quand même un gagnant ! " );
+  printf(" Le classement des joueurs est donc : \n");
+    for (int y = taille-1; y > 0; y--){
+    printf(" Le classement du joueur %s est : %d \n", tab[y].pseudo, k );
+    k++;
+    }
+  }
 }
 
