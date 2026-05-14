0123f1c780c4c4a927ba0b5152edf7c04bca1aad
ninibamby <nino.payet@etu.u-cergy.fr>
2026-05-14 16:33:43 +0200

    Update affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -5,7 +5,7 @@
 
 #include"structcarte.h"
 
-void afficherCarte(Carte c) {
+void afficherCarte(carte c) {
 
 int couleur;
 if(c.type == 0){
@@ -49,7 +49,7 @@ if(c.type == 0){
         couleur = 90;
     }
 }
-else if(type == 1 || type == 2){
+else if(c.type == 1 || c.type == 2){
     couleur = 33;
 }
 
@@ -66,7 +66,7 @@ if(type == 0){
 
     printf("\033[0m ");
 }
-else if (type == 1 || type == 2){
+else if (c.type == 1 || c.type == 2){
      printf("\033[%dm", couleur);
 
     printf("╔═══════╗\n");  
@@ -88,9 +88,9 @@ else if (type == 1 || type == 2){
 void affiche_main( joueur j){
 printf(" votre main, %s est : \n ", j.pseudo );
 for ( int i = 0; i < j.nb_carte; i++ ){
-    afficherCarte(main[i]);
+    afficherCarte(j.main[i]);
 }
-printf(" \n ");
+printf("\n");
 }
 
 
@@ -109,10 +109,10 @@ int comp1;
 for (int i=0 ; i<n ;i++){        //compte les 0 et 1
 comp0=strcmp("0",tab[i].nom);
 comp1=strcmp("1",tab[i].nom);
-  if (tab[i].type==0 && comp0=0){
+  if (tab[i].type == 0 && comp0 == 0){
   count0++;
   }
-  if (tab[i].type==0 && comp1=0){
+  if (tab[i].type == 0 && comp1 == 0){
   count1++;
   }
 }
@@ -120,33 +120,34 @@ comp1=strcmp("1",tab[i].nom);
 printf (" il reste %d %c \n",count0,'0');
 printf (" il reste %d %c \n",count1,'1');
 
-
-for (int i=0 ; i<n ;i++){        //compte les carte de 2 a 12 
-  for (int k=2 ; k<=12 ; k++){
-      int count=0;
+for ( int k = 2 ; k <= 12 ; k++ ){              //compte les carte de 2 a 12 
+     int count=0;
+for ( int i = 0 ; i < n ;i++ ){        
+  
       char nom_c[3];
-      if (tab[i].type==0 && tab[i].val==k){
+      if (tab[i].type == 0 && tab[i].val == k){
         count++;
        strcpy(nom_c,tab[i].nom);
         
       }
+    }
 printf (" il reste %d %s \n",count,nom_c);
-  }
-
 } 
 
+for (int k=2 ; k<=10 ; k+=2 ){
+    int count2=0;
 for (int i=0 ; i<n ;i++){        //compte les bonus de type +
-  for (int k=2 ; k<=10 ; k+2){
-      int count2=0;
+    
+  
       char nom_c[3];
       if (tab[i].type==1 && tab[i].val==k){
-        count++;
+        count2++;
        strcpy(nom_c,tab[i].nom);
         
       }
-printf (" il reste %d %s \n",count2,nom_c);
-  }
 
+  }
+printf (" il reste %d %s \n",count2,nom_c);
 } 
 
 int countX2=0;
@@ -156,5 +157,5 @@ for (int i=0 ; i<n ;i++){        //compte les X2
   countX2++;
   }
 }
-printf (" il reste %d %c \n",countX2,"X2");
+printf (" il reste %d %s \n",countX2,"X2");
 }
