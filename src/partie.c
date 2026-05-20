687a794fbd99b2ac06846aa00ceaa99fa2456821
ninibamby <nino.payet@etu.u-cergy.fr>
2026-05-20 10:19:07 +0200

    Update partie.c

--- src/partie.c
+++ src/partie.c
@@ -206,3 +206,9 @@ printf(" ----------- FIN DE LA PARTIE -------- \n");
 
 }
 
+void viderBuffer() {         // supprime la saisie de l'utilisateur si celle-ci est mauvaise
+  char c;
+  do {
+    c = getchar();
+  } while (c != '\n');
+}
