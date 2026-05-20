a573b24405e56acae1ccd6e1cea12b78b8cff93e
valzizi <valent1tag@gmail.com>
2026-05-20 11:24:59 +0200

    Update affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -45,8 +45,8 @@ printf("│                              │\n");
 printf("\033[0m");
 
 printf(CYAN"│"RESET);
-printf("Main de %-20.20s\n", j.pseudo);
-printf(CYAN"│"RESET);
+printf("Main de %-20.20s", j.pseudo);
+printf(CYAN"│\n"RESET);
 
 printf("\033[36m");
 printf("│                              │\n");
